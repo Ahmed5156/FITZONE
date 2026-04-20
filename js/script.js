b69d359cc9f9349a50dfc6524552695150d5afcf
Ahmed5156 <mgrani569@gmail.com>
2026-04-21 00:02:51 +0530

    Refactor slider functionality to use event listeners

--- js/script.js
+++ js/script.js
@@ -3,18 +3,20 @@ let index = 0;
 const slider = document.querySelector('.hero-slider');
 
 const images = [
-  "images/4.jpeg",
-  "images/3.jpeg",
+  "images/1.jpeg",
   "images/2.jpeg",
-  "images/1.jpeg"
+  "images/3.jpeg",
+  "images/4.jpeg"
 ];
 
-document.querySelector('.next').onclick = () => {
-  index = (index + 1) % images.length;
-  slider.style.backgroundImage = `url('${images[index]}')`;
-};
+document.querySelector('.next').addEventListener('click', () => {
+  index++;
+  if (index >= images.length) index = 0;
+  slider.style.backgroundImage = `url(${images[index]})`;
+});
 
-document.querySelector('.prev').onclick = () => {
-  index = (index - 1 + images.length) % images.length;
-  slider.style.backgroundImage = `url('${images[index]}')`;
-};
+document.querySelector('.prev').addEventListener('click', () => {
+  index--;
+  if (index < 0) index = images.length - 1;
+  slider.style.backgroundImage = `url(${images[index]})`;
+});
